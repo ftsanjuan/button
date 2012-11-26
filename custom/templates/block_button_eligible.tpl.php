c95b7607bc374a3905ff17a4c8ea159fda48ddbe
Francis San Juan <fsanjuan@commix.ca>
2012-11-26 10:21:17 -0500

    Initial commit.

---
+++ custom/templates/block_button_eligible.tpl.php
@@ -0,0 +1,4 @@
+<div class="container-daily-prize button-eligible">
+  <?php print $prize; ?>
+</div>
+<?php print $form; ?>
---
+++ custom/templates/block_button_ineligible.tpl.php
@@ -0,0 +1,21 @@
+<div class="container-daily-prize button-ineligible">
+
+  <div class="row">
+    <div class="span6">
+      <div class="message-pre-inner">
+        <?php print $message_pre; ?>
+      </div>
+    </div>
+  </div>
+
+  <?php print $prize; ?>
+
+  <div class="row">
+    <div class="span6">
+      <div class="message-post-inner">
+        <?php print $message_post; ?>
+      </div>
+    </div>
+  </div>
+
+</div>
